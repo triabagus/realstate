cbebeffa8a899577914407a6bfa177f9a1fecc85
triabagus <triatop9@gmail.com>
2022-10-27 22:39:46 +0700

    Change bg header scroll, logos section, popular section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,24 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the tag
+    if(this.scrollY >= 50 ) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER POPULAR ===============*/
+var swiperPopular = new Swiper(".popular__container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    sliderPerView: 'auto',
+    loop: true,
 
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+})
 
 /*=============== VALUE ACCORDION ===============*/
 
