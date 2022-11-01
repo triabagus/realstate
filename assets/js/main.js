c1e97c13a4c3e029d75f7d494338121974ab1441
triabagus <triatop9@gmail.com>
2022-11-01 21:37:08 +0700

    scroll up action

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,7 +71,12 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with scroll-up
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll') 
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
